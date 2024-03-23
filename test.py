f4b2b791dc81c9a99fd0f5b5dd35910a44b51e85
Takuan1805 <159244526+Takuan1805@users.noreply.github.com>
2024-03-23 18:04:10 +0900

    1st commit

---
+++ test.py
@@ -0,0 +1,18 @@
+num = 3
+print("1st check")
+print("num's value is " + str(num) + ". num's type is "+ str(type(num)) + ".")
+
+num = 4.5
+print("2nd check")
+print("num's value is " + str(num) + ". num's type is "+ str(type(num)) + ".")
+
+num = -6
+print("3rd check")
+print("num's value is " + str(num) + ". num's type is "+ str(type(num)) + ".")
+
+num = 7.5
+print("4th check")
+int(num)
+print("num's value is " + str(num) + ". num's type is "+ str(type(num)) + ".")
+
+print("5th check")
